f15c50bb668913ab8905a055f7fb25f368f09ea4
Fadriasya <nasrullahakbar18@gmail.com>
2025-04-20 14:58:59 +0700

    terbaru

--- app/Views/home.php
+++ app/Views/home.php
@@ -11,6 +11,6 @@
   <p class="lead">
     Kami adalah tempat berkumpulnya para pemikir, pembelajar, dan pencipta yang ingin tumbuh bersama dalam semangat kolaborasi. Di sini, setiap suara dihargai, setiap ide punya tempat, dan setiap anggota adalah bagian penting dari perjalanan ini.<br><br>
     KomunitasKu dibangun untuk menjadi ruang diskusi yang ramah, terbuka, dan inspiratif. Kamu bisa berbagi pemikiran, bertanya, memberi solusi, atau sekadar bersosialisasi dengan sesama anggota yang memiliki minat dan semangat yang sama.</p>
-  <a href="<?= base_url('register') ?>" class="btn btn-primary mt-3">Gabung Sekarang</a>
+  <a href="<?= base_url('register') ?>" class="btn btn-primary mt-3">Gabung Sekarang?</a>
 </div>
 <?= $this->endSection() ?>
