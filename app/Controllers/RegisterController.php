b5de350b900d16229630739fba2d2d103018100c
aditya100402 <107261020+aditya100402@users.noreply.github.com>
2025-06-27 17:05:40 +0700

    confirm password

--- app/Controllers/RegisterController.php
+++ app/Controllers/RegisterController.php
@@ -19,6 +19,8 @@ class RegisterController extends BaseController
       'username' => 'required|min_length[3]|is_unique[users.username]',
       'email'    => 'required|valid_email|is_unique[users.email]',
       'password' => 'required|min_length[5]',
+      'password_confirm' => 'required|matches[password]',
+      'role'     => 'permit_empty' // karena kamu isi manual, bukan dari form
     ]);
 
     if (!$validation->withRequest($this->request)->run()) {
@@ -30,6 +32,7 @@ class RegisterController extends BaseController
       'username' => $this->request->getPost('username'),
       'email'    => $this->request->getPost('email'),
       'password' => password_hash($this->request->getPost('password'), PASSWORD_DEFAULT),
+      'role'     => 'user',
     ]);
 
     return redirect()->to('/login')->with('message', 'Registrasi berhasil! Silakan login.');
